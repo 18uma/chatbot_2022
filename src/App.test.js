f9a1a0a3a2590848c868dcd71ac1a6a79ae613ae
18uma <yamamoto.y.bj@m.titech.ac.jp>
2022-04-26 18:10:09 +0900

    create AnswersList

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {AnswersList} from "./components/index";
 
 test('renders learn react link', () => {
   render(<App />);
